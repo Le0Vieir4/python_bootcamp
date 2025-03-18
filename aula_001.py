59cedb19d35960a580fff296a6095ec925377851
Leo <lvm9508@gmail.com>
2025-03-18 06:41:04 -0300

    mixed

---
+++ aula_001.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+print("Hello, " + name + "!")
+
+if name == "leo":
+  age = input("How old are you? ")
+if age == "25":
+  print(f"You are {name} and have {age} years old.")
