098d48c57670e62de3a012c653fec73cb07e42f5
Leo <lvm9508@gmail.com>
2025-03-21 10:12:30 -0300

    create bank simulator

--- aula_001.py
+++
@@ -1,7 +0,0 @@
-name = input("What is your name? ")
-print("Hello, " + name + "!")
-
-if name == "leo":
-  age = input("How old are you? ")
-if age == "25":
-  print(f"You are {name} and have {age} years old.")
